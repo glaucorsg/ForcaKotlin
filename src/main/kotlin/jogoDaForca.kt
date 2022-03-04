19ddd72cca22608f5ca4c1a3d070f8d359675f6f
glaucorsg <glaucorsg@gmail.com>
2022-03-04 12:02:07 -0300

    Ajusta número de tentativas, revela acertos repetidos e finaliza o jogo

--- src/main/kotlin/jogoDaForca.kt
+++ src/main/kotlin/jogoDaForca.kt
@@ -1,13 +1,14 @@
 import java.util.*
+import kotlin.math.sign
 import kotlin.reflect.typeOf
 
 fun main() {
 
-    var desafio1: Desafio = Desafio("ARMARIO", "Guarda as coisas")
+    var desafio1: Desafio = Desafio("ROSA", "Um tipo de flor")
 
     var tentativa: Int = 0
 
-    var numTentativas = ((desafio1.palavra.length) / 2) + 2
+    var numTentativas = ((desafio1.palavra.length) / 2) + 3
 
     var letra: String
 
@@ -15,97 +16,70 @@ fun main() {
 
     var palavraSplit = desafio1.palavra.split(delimiter).slice(1..contaLetras(desafio1.palavra))
 
-    var distintas = mutableListOf<String>()
+    var distintas = linkedSetOf<String>()
 
-    var corretas = mutableListOf<String>()
+    var corretas = linkedSetOf<String>()
 
-    var erradas = mutableListOf<String>()
+    var erradas = linkedSetOf<String>()
 
-    var oculta = mutableListOf<String>()
-
-    var temporaria = mutableListOf<String>()
-
-    temporaria = palavraSplit as MutableList<String>
-
-    println("temporaria: ${temporaria}")
+    var oculta = arrayListOf<String>()
 
     var i = 0
-    var aux: Boolean = false
+
 
     for (l in palavraSplit) {
-        for (m in temporaria) {
-            println("l: ${l} -- m: ${m}")
-            if (l.equals(m)) {
-                aux = true
-            }
-        }
-        println(aux)
-        if (!aux) {
-            println("passou aqui")
+        if (l in palavraSplit[i]) {
             distintas.add(l)
+            i++
         }
     }
-//        if(palavraSplit[i] == l){
-//            println("palavraSplit[${i}]: ${palavraSplit[i]} --- l: ${l}")
-//            i++
-
-//        } else {
-//            distintas.add(palavraSplit[i])
-//            i++
-//        }
-
-
-
-
-    println("distintas: ${distintas}")
-    println("palavraSplit: ${ palavraSplit }")
 
     for(l in palavraSplit) {
         oculta.add("*")
     }
 
-    println(
-        "Olá, este é o jogo da FORCA. Você deve advinhar a palavra secreta.\n\n" +
-                "A DICA é: ${desafio1.dica}\n")
+    println("Olá, este é o jogo da FORCA.\n")
+    println("A DICA é: ${desafio1.dica}\n")
 
     println("A palavra secreta contém ${contaLetras(desafio1.palavra)} letras sendo ${distintas.size} delas distintas.")
     println("Você tem um total de ${numTentativas} tentativas. \n")
 
     var j = 0
-    while (tentativa < numTentativas) {
+    while (tentativa < numTentativas && palavraSplit != oculta) {
         println("Essa é a sua ${tentativa+1}º tentativa, você possui mais ${ numTentativas- tentativa} chance(s)\n")
         println("Status do jogo: ${oculta}\n")
         println("Digite uma letra ou FIM para sair: \n")
         letra = readLine().toString().uppercase(Locale.getDefault())
 
-        println(letra)
-        println(palavraSplit)
+        var frequencia = 0
 
-            if (letra in palavraSplit) {
-                tentativa++
-                for (l in palavraSplit) {
-                    var index = palavraSplit.indexOf(letra)
-                    println("Você acertou a letra ${letra}")
-                    corretas.add(letra)
-                    oculta.set(index, letra.toString())
-                    j++
-                    println(oculta)
-                }
+        for(i in 0..palavraSplit.size -1) {
+            if (letra == palavraSplit[i]) {
+                frequencia++
+                oculta[i] = letra
             }
+        }
 
-            else {
-                println("A letra ${letra} não está presente na palavra")
-                erradas.add(letra)
-                tentativa++
-            }
+        if (letra in palavraSplit) {
+            println("parabéns você acertou a letra ${letra}")
+            corretas.add(letra)
+            tentativa++
+        }
+        else {
+            println("A letra ${letra} não está presente na palavra")
+            erradas.add(letra)
+            tentativa++
+        }
+
+        println("ACERTOS: ${corretas}")
+        println("ERROS: ${erradas}")
     }
-
-
-
-
-    println("ACERTOS: ${corretas}")
-    println("ERROS: ${erradas}")
-
+    println("FIM DO JOGO")
+    if(erradas.size == numTentativas){
+        println("GAME OVER")
+    } else if (palavraSplit. equals(oculta)){
+        println("PARABÉNS, VOCÊ GANHOU")
+    } else null
 }
 
 
